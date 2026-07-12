c561b28096dc3d54b714f871b8b5e62f480d0047
yihuang <yi.codeplayer@gmail.com>
2026-07-12 15:28:23 +0800

    Problem: implicit instance name is confusing
    define instance name explicitly.

--- EvmAbi/Encode.lean
+++ EvmAbi/Encode.lean
@@ -49,7 +49,7 @@ def tuplePack (headSizes : List Nat) (dynamics : List Bool) (encoded : List (Boo
       ) init encoded
     heads ++ tails
 /-! ## The ABIVisitor instance for encoding. -/
-@[simp] instance : ABIVisitor EncoderEntry where
+@[simp] instance instABIVisitorDecoderEntry: ABIVisitor EncoderEntry where
   onUint s := (false, λ v => match v with
     | .uint v' =>
       let b := s.len * 8
